cd6c7dff0fac0e018de81a47d77fde661dd966ea
ollienorman <oliver.norman1991@gmail.com>
2020-11-30 15:58:17 +0000

    challenge 6 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,22 @@
 class Oystercard
 
   attr_reader :balance
+  BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Limit of #{Oystercard::BALANCE_LIMIT} reached!" if limit(amount)
     @balance += amount
     @balance
+  end
+
+  private
+
+  def limit(amount)
+    amount + @balance > BALANCE_LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
-
   it "starts with a default balance of 0" do
     expect(subject.balance).to eq 0
   end
 
-  it "can be topped up by a custom amount" do
-    expect(subject.top_up(5)).to eq 5
-  end
+  describe "#top_up" do
+    it "can be topped up by a custom amount" do
+      expect(subject.top_up(5)).to eq 5
+    end
 
+    it "raises an error when balance exceeds limit" do
+      expect { subject.top_up(91) }.to raise_error "Limit of #{Oystercard::BALANCE_LIMIT} reached!"
+    end
+  end
 end
