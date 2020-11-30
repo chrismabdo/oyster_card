b9984c0f4a9d79ae215764f2648a4fe795736001
Chris Abdo <christophermabdo@icloud.com>
2020-11-30 16:50:32 +0000

    Chris commits step 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,34 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
+    @in_journey
   end
 
   def top_up(amount)
     fail "Limit of #{Oystercard::BALANCE_LIMIT} reached!" if limit(amount)
     @balance += amount
     @balance
+  end
+
+  def deduct(amount)
+    @balance -= amount
+    @balance
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,31 @@ describe Oystercard do
       expect { subject.top_up(91) }.to raise_error "Limit of #{Oystercard::BALANCE_LIMIT} reached!"
     end
   end
+
+  it "should deduct balance by a specified amount" do
+    subject.top_up(10)
+    expect(subject.deduct(10)).to eq 0
+  end
+
+  describe "#touch_in" do
+    it "should confirm that the card has been touched in" do
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
+  end
+
+  describe "#touch_out" do
+    it "should confirm that the card has been touched out" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
+  end
+
+  describe "#in_journey" do
+    it "should check whether the card is in journey or not" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
 end
